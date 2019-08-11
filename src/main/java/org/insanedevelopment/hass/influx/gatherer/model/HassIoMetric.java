caed0658abf51e3b737eca3343cfcaae8cf2bdac
choss <clayton.hoss@googlemail.com>
2019-08-11 20:48:11 +0200

    na: finished converting to metrics

--- src/main/java/org/insanedevelopment/hass/influx/gatherer/model/HassIoMetric.java
+++ src/main/java/org/insanedevelopment/hass/influx/gatherer/model/HassIoMetric.java
@@ -1,15 +1,77 @@
 package org.insanedevelopment.hass.influx.gatherer.model;
 
-import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class HassIoMetric {
-	private Map<String, String> attributes;
-	private Map<String, BigDecimal> measurements;
+	private Map<String, String> tags = new HashMap<>();
+	private Map<String, String> measurements = new HashMap<>();
 
 	private String entityId;
 	private String domain;
 	private String entityName;
 	private String friendlyName;
 
+	public HassIoMetric(String entityId) {
+		this.entityId = entityId;
+		domain = StringUtils.substringBefore(entityId, ".");
+		entityName = StringUtils.substringAfter(entityId, ".");
+	}
+
+	public void addMeasurement(String attributeName, String attributeValue) {
+		measurements.put(attributeName, attributeValue);
+	}
+
+	public void addTag(String attributeName, String attributeValue) {
+		tags.put(attributeName, attributeValue);
+	}
+
+	public Map<String, String> getTags() {
+		return tags;
+	}
+
+	public Map<String, String> getMeasurements() {
+		return measurements;
+	}
+
+	public String getEntityId() {
+		return entityId;
+	}
+
+	public String getDomain() {
+		return domain;
+	}
+
+	public String getEntityName() {
+		return entityName;
+	}
+
+	public String getFriendlyName() {
+		return friendlyName;
+	}
+
+	public void setFriendlyName(String friendlyName) {
+		this.friendlyName = friendlyName;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("HassIoMetric [entityName=");
+		builder.append(entityName);
+		builder.append(", entityId=");
+		builder.append(entityId);
+		builder.append(", friendlyName=");
+		builder.append(friendlyName);
+		builder.append(", tags=");
+		builder.append(tags);
+		builder.append(", measurements=");
+		builder.append(measurements);
+		builder.append(", domain=");
+		builder.append(domain);
+		builder.append("]");
+		return builder.toString();
+	}
 }
--- src/main/java/org/insanedevelopment/hass/influx/gatherer/service/InfluxDbSendingService.java
+++ src/main/java/org/insanedevelopment/hass/influx/gatherer/service/InfluxDbSendingService.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.insanedevelopment.hass.influx.gatherer.model.FilterPathSpec;
+import org.insanedevelopment.hass.influx.gatherer.model.HassIoMetric;
 import org.insanedevelopment.hass.influx.gatherer.model.json.HassIoState;
 import org.insanedevelopment.hass.influx.gatherer.repository.ConfigRepositoryImpl;
 import org.insanedevelopment.hass.influx.gatherer.repository.HassioRestStateClientImpl;
@@ -49,19 +52,55 @@ public class InfluxDbSendingService {
 		Flux<HassIoState> repositoryResult = restClient.getAllCurrentStates();
 
 		repositoryResult
-				.filter(has -> shouldBeReported(has.getEntityId()))
-				.doOnNext(ha -> LOGGER.info("Matched {}", ha))
+				.map(has -> addSpec(has))
+				.filter(p -> p.getRight() != null)
+				.doOnNext(ha -> LOGGER.info("Matched {}", ha.getLeft()))
+				.map(this::convertToMetrics)
+				.doOnNext(ha -> LOGGER.info("Reporting {}", ha))
 				.subscribe();
 	}
 
-	private boolean shouldBeReported(String entityId) {
+	private Pair<HassIoState, FilterPathSpec> addSpec(HassIoState state) {
+		String entityId = state.getEntityId();
 		List<FilterPathSpec> configElements = configRepository.getAllConfigElements();
 		for (FilterPathSpec filterPathSpec : configElements) {
 			if (filterPathSpec.matches(entityId)) {
-				return true;
+				return Pair.of(state, filterPathSpec);
 			}
 		}
-		return false;
+		return Pair.of(state, null);
+	}
+
+	private HassIoMetric convertToMetrics(Pair<HassIoState, FilterPathSpec> item) {
+		HassIoState state = item.getLeft();
+		FilterPathSpec spec = item.getRight();
+
+		HassIoMetric result = new HassIoMetric(state.getEntityId());
+
+		result.setFriendlyName(String.valueOf(state.getAttribute("friendly_name")));
+
+		for (String attributeName : spec.getCaptureAttributes()) {
+			Object attributeValue = state.getAttribute(attributeName);
+			if (attributeValue != null) {
+				result.addMeasurement(attributeName, attributeValue.toString());
+			}
+		}
+
+		for (String attributeName : spec.getTagAttributes()) {
+			Object attributeValue = state.getAttribute(attributeName);
+			if (attributeValue != null) {
+				result.addTag(attributeName, attributeValue.toString());
+			}
+		}
+
+		String stateName = "state";
+		if (!StringUtils.isBlank(spec.getOverwriteStateFieldWithAttribute())) {
+			stateName = state.getAttribute(spec.getOverwriteStateFieldWithAttribute()).toString();
+		}
+
+		result.addMeasurement(stateName, state.getState());
+
+		return result;
 	}
 
 }
